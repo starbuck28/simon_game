b6574511b9aa2b9bb74f1affdfbd63f6eadf20ba
Elaine Oglesbay <elaine.oglesbay@gmail.com>
2017-03-06 19:45:47 -0500

    First commit

---
+++ src/js/simon.js
@@ -0,0 +1,9 @@
+//It has a start button
+//It has a stop button
+//It has a yellow button, a red button, a green button, and a blue button
+//It generates a random pattern counting up from 1
+//It lights up the buttons with random generated pattern
+//It recognizes a colored button push
+//It compares computer generated pattern to pattern entered by user
+  //If correct, continues advancing pattern
+  //If incorrect, alerts player pattern is wrong
---
+++ src/spec/simon_spec.js
@@ -0,0 +1,3 @@
+describe("Simon JavaScript Game", function() {
+
+});
