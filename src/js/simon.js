ff40e7355a6b0128186dd2185ac1aa654c5507a2
Elaine Oglesbay <elaine.oglesbay@gmail.com>
2017-03-07 11:17:21 -0500

    created and tested first function to generate a random number

--- src/js/simon.js
+++ src/js/simon.js
@@ -11,33 +11,37 @@
   //The computer generates a random number corresponding to a color (between 0 and 3)
   //The number is pushed to an array keeping track of the pattern of colors
   //The corresponding color lights up
+  //A corresponding sound plays
 //The computer waits for the player to match the pattern
+  //When each colored button is clicked
+    //A sound corresponding to the button is heard
+    //The button lights up
+    //The button's number is added to a player array
   //If the pattern doesn't match
     //All 4 buttons flash twice
     //A negative sound is heard
     //If the strict button has been activated, the game starts over
+    //If the strict button is not active, the computer's pattern lights up again
   //If the pattern does match
     //Buttons light up in a circle
     //A positive sound is heard
     //The counter increases by 1
     //The computer generates a random color and adds to the pattern
+  //When the player's array reaches 20
+    //The player is alerted of their win
+    //The game starts over
   //When the OFF button is pressed, the game resets
     //All arrays are cleared, counter is set to 0, counter screen is blank
 
-//It generates a random pattern counting up from 1
-  //Computer generates a random number between 0 and 3
-  //Computer pushes number into existing array
-  //Computer tracks length of array
-//It lights up the buttons with random generated pattern
-  //Computer iterates through array and lights up corresponding color for each index
-//It recognizes a colored button push
-  //When player clicks on button
-    //Lights up color
-    //Pushes number of clicked button to existing array
-//It compares computer generated pattern to pattern entered by user
-  //If correct, continues advancing pattern
-  //If incorrect, alerts player pattern is wrong
+
 
   var Simon = (function() {
-    return true;
+    function startGame() {
+      return Math.floor(Math.random()) * 3;
+    }
+
+    return {
+      startGame: startGame
+    };
+
   })();
--- src/spec/simon_spec.js
+++ src/spec/simon_spec.js
@@ -1,5 +1,8 @@
 describe("Simon JavaScript Game", function() {
   it("is defined", function() {
-    
+    expect(Simon).toBeDefined();
+  });
+  it("should contain a game start function that generates a random number between 0 and 3", function() {
+    expect(Simon.startGame()).toBeLessThan(4);
   });
 });
