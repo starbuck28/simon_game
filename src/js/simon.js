0c87cb107f3b1931aaa98ce9c10a463e124deb44
Elaine Oglesbay <elaine.oglesbay@gmail.com>
2017-03-06 20:06:42 -0500

    planning code

--- src/js/simon.js
+++ src/js/simon.js
@@ -1,8 +1,13 @@
+
 //It has a start button
 //It has a stop button
 //It has a yellow button, a red button, a green button, and a blue button
 //It generates a random pattern counting up from 1
+  //Computer generates a random number between 0 and 3
+  //Computer pushes number into existing array
+  //Computer tracks length of array
 //It lights up the buttons with random generated pattern
+  //Computer iterates through array and lights up corresponding color for each index
 //It recognizes a colored button push
 //It compares computer generated pattern to pattern entered by user
   //If correct, continues advancing pattern
